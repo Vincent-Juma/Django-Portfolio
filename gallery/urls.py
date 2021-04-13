c5c341ef846b911e7315bf18a5ca04a83c57872f
Vincent-Juma <vincentjuma64@gmail.com>
2021-04-13 14:07:58 +0300

    update url

--- gallery/urls.py
+++ gallery/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path, include
+from django.urls import , url, path, include
 from users import views as user_views
 from django.conf.urls.static import static
 from django.conf import settings
