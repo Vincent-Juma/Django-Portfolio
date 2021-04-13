24da6c1e873c9d3ca0858ba45740402d0d05fa68
Vincent-Juma <vincentjuma64@gmail.com>
2021-04-13 14:10:15 +0300

    update settings

--- gallery/settings.py
+++ gallery/settings.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 import django_heroku
 import dj_database_url
-from decouple import config
+# from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
