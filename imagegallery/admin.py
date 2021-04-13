050f7b74c72b79100474f796fbeb52afc952b3a1
Vincent-Juma <vincentjuma64@gmail.com>
2021-04-13 13:58:17 +0300

    update admin.py

--- imagegallery/admin.py
+++ imagegallery/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from imagegallery.models import imggal
+from imagegallery.models import imggal, Image,Location,Category
 
 admin.site.register(imggal)
+admin.site.register(Image)
+admin.site.register(Location)
+admin.site.register(Category)
