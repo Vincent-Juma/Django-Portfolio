7a99448f7f45993b4d88f9928b00f94e43e1ac01
Vincent-Juma <vincentjuma64@gmail.com>
2021-04-13 13:55:03 +0300

    update urls.py

--- imagegallery/urls.py
+++ imagegallery/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -7,4 +11,10 @@ urlpatterns = [
     path('contacts/', views.contacts, name='contacts'),
     path('info/', views.info, name='info'),
     path('career/', views.career, name='career'),
+    url(r'^$',views.index,name='index'),
+    url(r'^location/(\d+)',views.display_location,name='displayLocation'),
+    url(r'^search/',views.search_category,name='search_category')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
